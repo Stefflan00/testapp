1e886919e94ca05f745c22c77d63a14e9f0142e7
Stefflan00 <stefan.maier09@gmail.com>
2014-12-12 13:48:19 +0100

    with pem

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,7 @@ role :db,  %w{deploy@54.171.181.15}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+#server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 
 # Custom SSH Options
@@ -24,11 +24,11 @@ server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+set :ssh_options, {
+    keys: %w(/Users/stefanmaier/Downloads/data_tool_server.pem),
+    #forward_agent: false,
+    #auth_methods: %w(password)
+  }
 #
 # And/or per server (overrides global)
 # ------------------------------------
